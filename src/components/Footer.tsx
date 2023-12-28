ebb1b05e046fe801925fed7039ad1be01f320c0d
scott-jack <scottjack2004@gmail.com>
2023-12-28 12:31:43 -0800

    fixing images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,8 +15,8 @@ interface Props {
           <span className="mb-3 mb-md-0 text-body-secondary">Made by Jack Scott (with React, Vite, and Bootstrap)</span>
         </div>
         <ul className="nav col-md-4 justify-content-center list-unstyled d-flex">
-          <li className="ms-3"><a className="text-body-secondary" href="https://www.linkedin.com/in/jack-w-scott/"><img className="bi" src="../public/linkedin.svg" alt="" width="30" height="24"></img></a></li>
-          <li className="ms-3"><a className="text-body-secondary" href="https://github.com/scott-jack"><img className="bi" src="../public/github.svg" alt="" width="30" height="24"></img></a></li>
+          <li className="ms-3"><a className="text-body-secondary" href="https://www.linkedin.com/in/jack-w-scott/"><img className="bi" src="./public/linkedin.svg" alt="" width="30" height="24"></img></a></li>
+          <li className="ms-3"><a className="text-body-secondary" href="https://github.com/scott-jack"><img className="bi" src="./public/github.svg" alt="" width="30" height="24"></img></a></li>
         </ul>
       </footer>
     );
